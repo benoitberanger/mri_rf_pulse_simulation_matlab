ce8aa3e7130bb395abf51d7cb5b7551305157814
Benoît Béranger <benoit.beranger@icm-institute.org>
2023-10-04 21:13:36 +0200

    sms_pins now uses gradient blips

--- +mri_rf_pulse_sim/+rf_pulse/+sms/@sms_pins/sms_pins.m
+++ +mri_rf_pulse_sim/+rf_pulse/+sms/@sms_pins/sms_pins.m
@@ -12,11 +12,30 @@ classdef sms_pins < mri_rf_pulse_sim.backend.rf_pulse.abstract
     end % props
 
     properties (GetAccess = public, SetAccess = protected, Dependent)
-        bandwidth                                                          % Hz
+        slice_distance                                                     % [m]
+        bandwidth                                                          % [Hz]
+        subpulse_number                                                    % []
+        blip_duration                                                      % [s]
     end % props
 
     methods % no attribute for dependent properies
-        function value = get.bandwidth(self); value = 1 /(self.N * self.duration / (self.M * 2 + 1)); end
+
+        function value = get.slice_distance(self)
+            value = self.N * self.slice_thickness;
+        end
+
+        function value = get.bandwidth(self)
+            value = self.subpulse_number /(self.N * self.duration);
+        end
+
+        function value = get.subpulse_number(self)
+            value = 2 * self.M + 1;
+        end
+
+        function value = get.blip_duration(self)
+            value = self.duration / (self.subpulse_number-1) - self.subpulse_duration;
+        end
+
     end % meths
 
     methods (Access = public)
@@ -25,7 +44,7 @@ classdef sms_pins < mri_rf_pulse_sim.backend.rf_pulse.abstract
         function self = sms_pins()
             self.flip_angle        = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='flip_angle'       , value= 90       , unit='°'              );
             self.N                 = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='N'                , value=  2                               );
-            self.M                 = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='M'                , value=  4                               );
+            self.M                 = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='M'                , value= 10                               );
             self.subpulse_duration = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='subpulse_duration', value=100 * 1e-6, unit='us'  , scale=1e6);
             self.generate_PINS();
         end % fcn
@@ -51,7 +70,14 @@ classdef sms_pins < mri_rf_pulse_sim.backend.rf_pulse.abstract
             waveform = waveform * deg2rad(self.flip_angle.get()) / self.gamma; % scale integrale with flip angle
 
             self.B1  = waveform;
-            self.GZ  = ones(size(self.time)) * self.GZavg;
+
+            gradientshape = zeros(size(self.time));
+            for m = 1 : self.M.get()
+                signal = (self.blip(-(m-0.5)/self.M*self.duration/2) + self.blip(+(m-0.5)/self.M*self.duration/2));
+                gradientshape = gradientshape + signal;
+            end
+            self.GZ = self.GZavg / mean(gradientshape) * gradientshape; % scale gradient -> for slice thickness
+            % self.GZ = ones(size(self.time)) * self.GZavg; % uncomment if you want to use continuous slice gradient => slice profile is disgusting
 
         end % fcn
 
@@ -81,12 +107,34 @@ classdef sms_pins < mri_rf_pulse_sim.backend.rf_pulse.abstract
             [~, pos_idx] = min(abs(self.time-position));
             subpulse_points = round(self.n_points * self.subpulse_duration/self.duration);
 
-            idx = round(pos_idx + (-subpulse_points/2 : +subpulse_points/2));
+            idx = round(pos_idx + (1:subpulse_points) - subpulse_points/2);
             idx(idx<=0                  ) = [];
             idx(idx >self.n_points.get()) = [];
             if ~isempty(idx)
                 signal(idx) = 1;
             end
+        end
+
+        function signal = blip(self, position)
+            if nargin < 2
+                position = 0;
+            end
+
+            signal = zeros(size(self.time));
+            [~, pos_idx] = min(abs(self.time-position));
+            blip_points = round(self.n_points * self.blip_duration/self.duration);
+
+            idx = round(pos_idx + (1:blip_points) - blip_points/2);
+
+            shape = linspace(0,blip_points/2,blip_points/2);
+            if mod(blip_points,2) == 0
+                shape = [shape fliplr(shape)];
+            else
+                shape = [shape blip_points/2 fliplr(shape)];
+            end
+            shape = shape / (blip_points/2);
+
+            signal(idx) = shape;
         end
 
     end % meths
