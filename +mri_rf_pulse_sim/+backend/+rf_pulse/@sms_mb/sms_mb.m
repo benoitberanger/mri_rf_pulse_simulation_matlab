f005415e769b02281a01b89e67bda30bf3edd401
Benoît Béranger <benoit.beranger@icm-institute.org>
2023-10-24 11:05:42 +0200

    new abstract class sms_mb
    sms_mb_sinc and sms_mb_hs adapted

---
+++ +mri_rf_pulse_sim/+backend/+rf_pulse/@sms_mb/sms_mb.m
@@ -0,0 +1,53 @@
+classdef (Abstract) sms_mb < mri_rf_pulse_sim.backend.rf_pulse.abstract
+    % Barth M, Breuer F, Koopmans PJ, Norris DG, Poser BA. Simultaneous
+    % multislice (SMS) imaging techniques. Magn Reson Med. 2016
+    % Jan;75(1):63-81. doi: 10.1002/mrm.25897. Epub 2015 Aug 26. PMID:
+    % 26308571; PMCID: PMC4915494.
+
+    properties (GetAccess = public, SetAccess = public)
+        n_slice        mri_rf_pulse_sim.ui_prop.scalar                     % []  number of slices, from 1 to +Inf
+        slice_distance mri_rf_pulse_sim.ui_prop.scalar                     % [m] distance between each slice
+    end % props
+
+    properties (GetAccess = public, SetAccess = protected, Dependent)
+        band_seperation (1,1) double                                       % Hz
+    end % props
+
+    methods % no attribute for dependent properies
+        function value = get.band_seperation(self)
+            value = self.bandwidth * self.slice_distance/self.slice_thickness;
+        end
+    end % meths
+
+    methods(Access = public)
+
+        % constructor
+        function self = sms_mb()
+            self.n_slice        = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='n_slice'       , value=3                             );
+            self.slice_distance = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='slice_distance', value=6 * 1e-3, unit='mm', scale=1e3);
+        end
+
+        function mb_phase_modulation(self)
+
+            % prepare slice offcet vector : integer number "representing" the slice index.
+            if mod(self.n_slice.get(),2) == 0
+                vect = linspace(-self.n_slice/2, self.n_slice/2, self.n_slice);
+            else
+                vect = linspace(-fix(self.n_slice/2), fix(self.n_slice/2), self.n_slice);
+            end
+
+            % compute the phase modution : each slice has its own delta of central frequency.
+            % central frequency + gradient = slice postion in space
+            % bandwidth + gradient = slice width
+            mb_phase_mudulation = 0;
+            for n = vect
+                mb_phase_mudulation = mb_phase_mudulation + exp(1j * 2*pi* self.band_seperation * n * self.time);
+            end
+
+            self.B1 = self.B1 .* mb_phase_mudulation;
+
+        end
+
+    end % meths
+
+end % class
--- +mri_rf_pulse_sim/+rf_pulse/+sms/@sms_mb_sinc/sms_mb_sinc.m
+++ +mri_rf_pulse_sim/+rf_pulse/+sms/@sms_mb_sinc/sms_mb_sinc.m
@@ -1,30 +1,9 @@
-classdef sms_mb_sinc < mri_rf_pulse_sim.rf_pulse.sinc
-    % Barth M, Breuer F, Koopmans PJ, Norris DG, Poser BA. Simultaneous
-    % multislice (SMS) imaging techniques. Magn Reson Med. 2016
-    % Jan;75(1):63-81. doi: 10.1002/mrm.25897. Epub 2015 Aug 26. PMID:
-    % 26308571; PMCID: PMC4915494.
-
-    properties (GetAccess = public, SetAccess = public)
-        n_slice        mri_rf_pulse_sim.ui_prop.scalar                     % []  number of slices, from 1 to +Inf
-        slice_distance mri_rf_pulse_sim.ui_prop.scalar                     % [m] distance between each slice
-    end % props
-
-    properties (GetAccess = public, SetAccess = protected, Dependent)
-        band_seperation (1,1) double                                       % Hz
-    end % props
-
-    methods % no attribute for dependent properies
-        function value = get.band_seperation(self)
-            value = self.bandwidth * self.slice_distance/self.slice_thickness;
-        end
-    end % meths
+classdef sms_mb_sinc < mri_rf_pulse_sim.rf_pulse.sinc & mri_rf_pulse_sim.backend.rf_pulse.sms_mb
 
     methods (Access = public)
 
         % constructor
         function self = sms_mb_sinc()
-            self.n_slice        = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='n_slice'       , value=3                             );
-            self.slice_distance = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='slice_distance', value=6 * 1e-3, unit='mm', scale=1e3);
             self.n_lobs.value = 3;
             self.generate_sms_mb_sinc();
         end % fcn
@@ -38,22 +17,8 @@ classdef sms_mb_sinc < mri_rf_pulse_sim.rf_pulse.sinc
             % generate SINC pulse : this is SingleBand pulse waveform
             self.generate_sinc();
 
-            % prepare slice offcet vector : integer number "representing" the slice index.
-            if mod(self.n_slice.get(),2) == 0
-                vect = linspace(-self.n_slice/2, self.n_slice/2, self.n_slice);
-            else
-                vect = linspace(-fix(self.n_slice/2), fix(self.n_slice/2), self.n_slice);
-            end
-
-            % compute the phase modution : each slice has its own delta of central frequency.
-            % central frequency + gradient = slice postion in space
-            % bandwidth + gradient = slice width
-            mb_phase_mudulation = 0;
-            for n = vect
-                mb_phase_mudulation = mb_phase_mudulation + exp(1j * 2*pi* self.band_seperation * n * self.time);
-            end
-
-            self.B1 = self.B1 .* mb_phase_mudulation;
+            % apply multi-band phase modulation to B1
+            self.mb_phase_modulation();
             % GM is already set using the SINC as base class
 
         end % fcn
