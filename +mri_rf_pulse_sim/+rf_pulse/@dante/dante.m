f629283922dd5a1498b41c22bd6b2bc53233ea7c
Benoît Béranger <benoit.beranger@icm-institute.org>
2023-11-03 12:52:50 +0100

    upgraded dante : use_blip, phase_alternation
    added Ref for DANTE

--- +mri_rf_pulse_sim/+rf_pulse/@dante/dante.m
+++ +mri_rf_pulse_sim/+rf_pulse/@dante/dante.m
@@ -1,10 +1,19 @@
 classdef dante < mri_rf_pulse_sim.backend.rf_pulse.abstract
-    % ref ?
+    % Morris GA, Freeman R. Selective excitation in Fourier transform
+    % nuclear magnetic resonance. 1978. J Magn Reson. 2011
+    % Dec;213(2):214-43. doi: 10.1016/j.jmr.2011.08.031. PMID: 22152346.
+    %
+    % Li, L., Miller, K.L. and Jezzard, P. (2012), DANTE-prepared pulse
+    % trains: A novel approach to motion-sensitized and motion-suppressed
+    % quantitative magnetic resonance imaging. Magn Reson Med, 68:
+    % 1423-1438. https://doi.org/10.1002/mrm.24142
 
     properties (GetAccess = public, SetAccess = public)
         flip_angle        mri_rf_pulse_sim.ui_prop.scalar                  % [deg] flip angle
         subpulse_number   mri_rf_pulse_sim.ui_prop.scalar                  % [] it affects th TBWP, hence the slice profile
         subpulse_duration mri_rf_pulse_sim.ui_prop.scalar                  % [s] duration of each RECT subpluse, high subpulse duration means high maximum gradient
+        phase_alternation mri_rf_pulse_sim.ui_prop.bool                    % [] keep phase at 0° for each subpulse, or alternate by 180°
+        use_blip          mri_rf_pulse_sim.ui_prop.bool                    % [] use blip gradients between subpulse, or use continuous slice gradient
     end % props
 
     properties (GetAccess = public, SetAccess = protected, Dependent)
@@ -15,7 +24,7 @@ classdef dante < mri_rf_pulse_sim.backend.rf_pulse.abstract
     methods % no attribute for dependent properies
 
         function value = get.bandwidth(self)
-            value = self.subpulse_number / self.duration;
+            value = 1 / self.duration;
         end
 
         function value = get.blip_duration(self)
@@ -31,6 +40,8 @@ classdef dante < mri_rf_pulse_sim.backend.rf_pulse.abstract
             self.flip_angle        = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='flip_angle'       , value= 90       , unit='°'                  );
             self.subpulse_number   = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='subpulse_number'  , value= 10                                   );
             self.subpulse_duration = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='subpulse_duration', value=100 * 1e-6, unit='us'      , scale=1e6);
+            self.phase_alternation = mri_rf_pulse_sim.ui_prop.bool  (parent=self, name='phase_alternation', value=true      , text='phase_alternation'  );
+            self.use_blip          = mri_rf_pulse_sim.ui_prop.bool  (parent=self, name='use_blip'         , value=true      , text='use_blip'           );
             self.generate_DANTE();
         end % fcn
 
@@ -43,8 +54,13 @@ classdef dante < mri_rf_pulse_sim.backend.rf_pulse.abstract
 
             self.time = linspace(0, self.duration.value, self.n_points.value);
 
-            B1 = zeros(size(self.time));
-            GZ = zeros(size(self.time));
+            MAG = zeros(size(self.time));
+            PHA = zeros(size(self.time));
+            if self.use_blip.get()
+                GZ = zeros(size(self.time));
+            else
+                GZ = ones(size(self.time));
+            end
 
             sample_subpulse = self.n_points * self.subpulse_duration/self.duration; % keep float
             sample_blip     = self.n_points * self.blip_duration    /self.duration; % keep float
@@ -54,6 +70,7 @@ classdef dante < mri_rf_pulse_sim.backend.rf_pulse.abstract
 
             gz_increase = 1 / ((sample_blip-2)/2);
             gz_slope_sign = +1;
+            phase_increament = +180; % in degree
 
             for idx = 1 : self.n_points.get()
 
@@ -65,25 +82,35 @@ classdef dante < mri_rf_pulse_sim.backend.rf_pulse.abstract
                 if n_subpulse == 0 % initilization : start with a subpulse
 
                     if idx <= sample_subpulse
-                        B1(idx) = 1;
+                        MAG(idx) = 1;
+                        if self.phase_alternation.get()
+                            PHA(idx) = phase_increament;
+                        end
                     else
                         n_subpulse = 1;
+                        phase_increament = -phase_increament;
                     end
 
                 elseif n_blip == n_subpulse % fill subpulse
 
                     if idx <= sample_subpulse*(n_subpulse+1) + sample_blip*n_blip
-                        B1(idx) = 1;
+                        MAG(idx) = 1;
+                        if self.phase_alternation.get()
+                            PHA(idx) = phase_increament;
+                        end
                     else
                         n_subpulse = n_subpulse + 1;
+                        phase_increament = -phase_increament;
                     end
 
                 elseif n_blip < n_subpulse % fill blip
 
                     if idx <= sample_subpulse*n_subpulse + sample_blip*(n_blip+1)
-                        GZ(idx) = GZ(idx-1) + gz_slope_sign*gz_increase;
-                        if GZ(idx) >= 1
-                            gz_slope_sign = -1;
+                        if self.use_blip.get()
+                            GZ(idx) = GZ(idx-1) + gz_slope_sign*gz_increase;
+                            if GZ(idx) >= 1
+                                gz_slope_sign = -1;
+                            end
                         end
                     else
                         n_blip = n_blip + 1;
@@ -95,9 +122,9 @@ classdef dante < mri_rf_pulse_sim.backend.rf_pulse.abstract
             end
 
             % scale waveform to the desired flip angle
-            B1 = B1 / trapz(self.time, B1); % normalize integral
-            B1 = B1 * deg2rad(self.flip_angle.get()) / self.gamma; % scale integrale with flip angle
-            self.B1  = B1;
+            MAG = MAG / trapz(self.time, MAG); % normalize integral
+            MAG = MAG * deg2rad(self.flip_angle.get()) / self.gamma; % scale integrale with flip angle
+            self.B1  = MAG .* exp(1j * deg2rad(PHA));
 
             % scale gradient -> for slice thickness
             self.GZ = self.GZavg / mean(GZ) * GZ;
@@ -111,9 +138,18 @@ classdef dante < mri_rf_pulse_sim.backend.rf_pulse.abstract
         end % fcn
 
         function init_specific_gui(self, container)
+            self.phase_alternation.add_uicontrol(...
+                container,...
+                [0.2 0.0 0.3 0.2]...
+                );
+            self.use_blip.add_uicontrol(...
+                container,...
+                [0.7 0.0 0.3 0.2]...
+                );
             mri_rf_pulse_sim.ui_prop.scalar.add_uicontrol_multi_scalar(...
                 container,...
-                [self.subpulse_number self.subpulse_duration self.flip_angle]...
+                [self.subpulse_number self.subpulse_duration self.flip_angle],...
+                [0.0 0.2 1.0 0.8]...
                 );
         end % fcn
 
