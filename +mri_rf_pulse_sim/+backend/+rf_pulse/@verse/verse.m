ae545f39852bb3ce923764af06a9b11254d789a3
Benoît Béranger <benoit.beranger@icm-institute.org>
2024-02-20 16:09:53 +0100

    verse gui scaffold

--- +mri_rf_pulse_sim/+backend/+rf_pulse/@verse/verse.m
+++ +mri_rf_pulse_sim/+backend/+rf_pulse/@verse/verse.m
@@ -10,12 +10,10 @@ classdef (Abstract) verse < mri_rf_pulse_sim.backend.rf_pulse.abstract
     methods(Access = public)
 
         function verse_rand(self)
-
             npts = self.n_points.get();
             dt = diff(self.time);
 
             ak = rand(1,npts);
-
             tv = [self.time(1)  (self.time(1) + cumsum(dt./ak(1:npts-1)))];
             bv = ak .* self.B1;
             gv = ak .* self.GZ;
@@ -23,7 +21,9 @@ classdef (Abstract) verse < mri_rf_pulse_sim.backend.rf_pulse.abstract
             self.time = tv;
             self.B1   = bv;
             self.GZ   = gv;
+        end % fcn
 
+        function init_verse_gui(self, container)
         end % fcn
 
     end % meths
