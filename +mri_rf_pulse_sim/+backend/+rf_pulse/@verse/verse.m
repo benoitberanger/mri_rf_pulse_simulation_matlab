617251af7f18dc0da80df7b2243588f8c7264c6c
Benoît Béranger <benoit.beranger@icm-institute.org>
2024-02-20 16:27:31 +0100

    verse gui & parameters implemented

--- +mri_rf_pulse_sim/+backend/+rf_pulse/@verse/verse.m
+++ +mri_rf_pulse_sim/+backend/+rf_pulse/@verse/verse.m
@@ -5,9 +5,20 @@ classdef (Abstract) verse < mri_rf_pulse_sim.backend.rf_pulse.abstract
     % https://doi.org/10.1016/0022-2364(88)90131-X
 
     properties (GetAccess = public, SetAccess = public)
+        type  mri_rf_pulse_sim.ui_prop.list
+        maxB1 mri_rf_pulse_sim.ui_prop.scalar                              % [T]     max value of magnitude(t)
+        maxGZ mri_rf_pulse_sim.ui_prop.scalar                              % [T/m]   max value of  gradient(t)
+        maxSZ mri_rf_pulse_sim.ui_prop.scalar                              % [T/m/s] max(dGZ/dt)
     end % props
 
     methods(Access = public)
+
+        function self = verse()
+            self.type  = mri_rf_pulse_sim.ui_prop.list  (parent=self, name='type' , value= 'rand' , items= {'minimum_time', 'low_sar', 'rand'});
+            self.maxB1 = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='maxB1', value= 15e-6, scale=1e6, unit='µT'     );
+            self.maxGZ = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='maxGZ', value= 40e-3, scale=1e3, unit='mT/m'   );
+            self.maxSZ = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='maxSZ', value=120e-3, scale=1e3, unit='mT/m/ms');
+        end % fcn
 
         function verse_rand(self)
             npts = self.n_points.get();
@@ -24,6 +35,11 @@ classdef (Abstract) verse < mri_rf_pulse_sim.backend.rf_pulse.abstract
         end % fcn
 
         function init_verse_gui(self, container)
+            self.type.add_uicontrol(container, [0.00 0.00 0.40 1.00]);
+            mri_rf_pulse_sim.ui_prop.scalar.add_uicontrol_multi_scalar(...
+                container,...
+                [self.maxB1 self.maxGZ self.maxSZ],...
+                [0.40 0.00 0.60 1.00]);
         end % fcn
 
     end % meths
