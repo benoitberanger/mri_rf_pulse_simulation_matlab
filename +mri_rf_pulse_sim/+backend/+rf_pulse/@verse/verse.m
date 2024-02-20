be81feb72f338b7006225d4af4b20c0789f75b13
Benoît Béranger <benoit.beranger@icm-institute.org>
2024-02-20 11:53:12 +0100

    verse (only RAND) is now an abstract class

---
+++ +mri_rf_pulse_sim/+backend/+rf_pulse/@verse/verse.m
@@ -0,0 +1,31 @@
+classdef (Abstract) verse < mri_rf_pulse_sim.backend.rf_pulse.abstract
+    % Steven Conolly, Dwight Nishimura, Albert Macovski, Gary Glover,
+    % Variable-rate selective excitation, Journal of Magnetic Resonance
+    % (1969), Volume 78, Issue 3, 1988, Pages 440-458, ISSN 0022-2364,
+    % https://doi.org/10.1016/0022-2364(88)90131-X
+
+    properties (GetAccess = public, SetAccess = public)
+    end % props
+
+    methods(Access = public)
+
+        function verse_rand(self)
+
+            npts = self.n_points.get();
+            dt = diff(self.time);
+
+            ak = rand(1,npts);
+
+            tv = [self.time(1)  (self.time(1) + cumsum(dt./ak(1:npts-1)))];
+            bv = ak .* self.B1;
+            gv = ak .* self.GZ;
+
+            self.time = tv;
+            self.B1   = bv;
+            self.GZ   = gv;
+
+        end % fcn
+
+    end % meths
+
+end % class
--- +mri_rf_pulse_sim/+rf_pulse/@sinc_verse/sinc_verse.m
+++ +mri_rf_pulse_sim/+rf_pulse/@sinc_verse/sinc_verse.m
@@ -1,11 +1,4 @@
-classdef sinc_verse < mri_rf_pulse_sim.rf_pulse.sinc
-    % Steven Conolly, Dwight Nishimura, Albert Macovski, Gary Glover,
-    % Variable-rate selective excitation, Journal of Magnetic Resonance
-    % (1969), Volume 78, Issue 3, 1988, Pages 440-458, ISSN 0022-2364,
-    % https://doi.org/10.1016/0022-2364(88)90131-X
-
-    properties (GetAccess = public, SetAccess = public)
-    end % props
+classdef sinc_verse < mri_rf_pulse_sim.rf_pulse.sinc & mri_rf_pulse_sim.backend.rf_pulse.verse
 
     methods (Access = public)
 
@@ -20,18 +13,12 @@ classdef sinc_verse < mri_rf_pulse_sim.rf_pulse.sinc
 
         function generate_sinc_verse(self)
             self.generate_sinc();
+            self.verse_rand();
+        end % fcn
 
-            dt = diff(self.time);
-            ak = rand(1,self.n_points.get());
-
-            tv = [self.time(1)  (self.time(1) + cumsum(dt./ak(1:self.n_points.get()-1)))];
-
-            bv = ak .* self.B1;
-            gv = ak .* self.GZ;
-
-            self.time = tv;
-            self.B1 = bv;
-            self.GZ = gv;
+        function txt = summary(self) % #abstract
+            txt = summary@mri_rf_pulse_sim.rf_pulse.sinc(self);
+            txt = strrep(txt,'[sinc]', sprintf('[%s::rand]',mfilename));
         end % fcn
 
     end % meths
