1cb2e7f9d22df764d11599d853cfac0def84bcd9
Benoît Béranger <benoit.beranger@icm-institute.org>
2024-03-21 12:17:55 +0100

    added foci_hs but hidden in the GUI/API
    just to make some checks

--- +mri_rf_pulse_sim/+rf_pulse/+verse/_@foci_verse/foci_verse.m
+++ +mri_rf_pulse_sim/+rf_pulse/+verse/_@foci_verse/foci_verse.m
@@ -1,35 +1,35 @@
-classdef hs_verse < mri_rf_pulse_sim.backend.rf_pulse.verse & mri_rf_pulse_sim.rf_pulse.hs
+classdef foci_verse < mri_rf_pulse_sim.backend.rf_pulse.verse & mri_rf_pulse_sim.rf_pulse.foci
 
     methods (Access = public)
 
         % constructor
-        function self = hs_verse()
-            self.generate_hs_verse();
+        function self = foci_verse()
+            self.generate_foci_verse();
         end % fcn
 
         function generate(self) % #abstract
-            self.generate_hs_verse();
+            self.generate_foci_verse();
         end % fcn
 
-        function generate_hs_verse(self)
-            self.generate_hs();
+        function generate_foci_verse(self)
+            self.generate_foci();
             self.verse_modulation();
         end % fcn
 
         function txt = summary(self) % #abstract
-            txt = summary@mri_rf_pulse_sim.rf_pulse.hs(self);
-            txt = strrep(txt,'[hs]', sprintf('[%s::%s]',mfilename,self.type.get()));
+            txt = summary@mri_rf_pulse_sim.rf_pulse.foci(self);
+            txt = strrep(txt,'[foci]', sprintf('[%s::%s]',mfilename,self.type.get()));
         end % fcn
 
         function init_specific_gui(self, container) % #abstract
-            pos_hs    = [0.00 0.00 0.30 1.00];
+            pos_foci  = [0.00 0.00 0.30 1.00];
             pos_verse = [0.30 0.00 0.70 1.00];
 
             fig_col = mri_rf_pulse_sim.backend.gui.get_fig_colors();
-            panel_hs = uipanel(container,...
-                'Title','hs',...
+            panel_foci = uipanel(container,...
+                'Title','foci',...
                 'Units','Normalized',...
-                'Position',pos_hs,...
+                'Position',pos_foci,...
                 'BackgroundColor',fig_col.figureBG);
             panel_verse = uipanel(container,...
                 'Title','verse',...
@@ -37,7 +37,7 @@ classdef hs_verse < mri_rf_pulse_sim.backend.rf_pulse.verse & mri_rf_pulse_sim.r
                 'Position',pos_verse,...
                 'BackgroundColor',fig_col.figureBG);
 
-            init_specific_gui@mri_rf_pulse_sim.rf_pulse.hs(self, panel_hs);
+            init_specific_gui@mri_rf_pulse_sim.rf_pulse.foci(self, panel_foci);
             self.init_verse_gui(panel_verse);
         end % fcn
 
