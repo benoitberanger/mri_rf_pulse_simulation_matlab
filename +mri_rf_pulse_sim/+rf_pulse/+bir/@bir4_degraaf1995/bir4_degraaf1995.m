108dcb2fad22bdbc5e9b1616f79d92e6a497a2fc
Benoît Béranger <benoit.beranger@icm-institute.org>
2024-04-02 16:45:27 +0200

    bir4_degraaf1995 : removed `n_transients`
    this does not make sense in the application
    it only makes sens in a full pulse sequence model, with lots of n_transients

--- +mri_rf_pulse_sim/+rf_pulse/+bir/@bir4_degraaf1995/bir4_degraaf1995.m
+++ +mri_rf_pulse_sim/+rf_pulse/+bir/@bir4_degraaf1995/bir4_degraaf1995.m
@@ -8,7 +8,6 @@ classdef bir4_degraaf1995 < mri_rf_pulse_sim.backend.rf_pulse.abstract
         flip_angle   mri_rf_pulse_sim.ui_prop.scalar                       % [deg] flip angle
         Beta         mri_rf_pulse_sim.ui_prop.scalar                       % []
         dWmax        mri_rf_pulse_sim.ui_prop.scalar                       % [] frequency sweep factor
-        n_transients mri_rf_pulse_sim.ui_prop.list                         % [] number of "transients"==BIR-4 with DeltaPhi in the article (phase cycling)
     end % props
 
     properties (GetAccess = public, SetAccess = protected, Dependent)
@@ -35,7 +34,6 @@ classdef bir4_degraaf1995 < mri_rf_pulse_sim.backend.rf_pulse.abstract
             self.flip_angle     = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='flip_angle'  , value=   90  , unit='°' );
             self.Beta           = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='Beta'        , value=    5.3           );
             self.dWmax          = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='dWmax'       , value= 3571  , unit='Hz');
-            self.n_transients   = mri_rf_pulse_sim.ui_prop.list  (parent=self, name='n_transients', items=[1,2,4], value=  1);
             self.generate_bir4_degraaf1995();
         end % fcn
 
@@ -45,8 +43,6 @@ classdef bir4_degraaf1995 < mri_rf_pulse_sim.backend.rf_pulse.abstract
 
         function generate_bir4_degraaf1995(self)
             % --- same terminology and notations as in the article ---
-
-            assert(self.n_transients.value == 1 || self.n_transients.value == 2 || self.n_transients.value == 4, 'n_transients must be 1, 2 or 4')
 
             Tp = self.duration.value;
             B1max = self.gammaB1max * 2*pi / self.gamma;
@@ -78,33 +74,10 @@ classdef bir4_degraaf1995 < mri_rf_pulse_sim.backend.rf_pulse.abstract
             PHA(c3) = PH(t3   - Tp/2);
             PHA(c4) = PH(t4   - Tp  );
 
-            switch self.n_transients.value
-                case 1
-                    PHA(c2) = PHA(c2) + deg2rad(self.delta_phase);
-                    PHA(c3) = PHA(c3) + deg2rad(self.delta_phase);
-                    self.time = t;
-                    self.B1 = MAG .* exp(1j * PHA);
-                case 2
-                    PHA_000 = PHA; % dont add phase term
-                    PHA_090 = PHA; % add 90°
-                    PHA_090(c2) = PHA_090(c2) + deg2rad(090);
-                    PHA_090(c3) = PHA_090(c3) + deg2rad(090);
-                    self.B1 = [MAG MAG] .* exp(1j * [PHA_000 PHA_090]);
-                    self.time = linspace(0, Tp*2, self.n_points.value*2);
-                case 4
-                    PHA_000 = PHA;
-                    PHA_090 = PHA;
-                    PHA_180 = PHA;
-                    PHA_270 = PHA;
-                    PHA_090(c2) = PHA_090(c2) + deg2rad(090);
-                    PHA_090(c3) = PHA_090(c3) + deg2rad(090);
-                    PHA_180(c2) = PHA_180(c2) + deg2rad(180);
-                    PHA_180(c3) = PHA_180(c3) + deg2rad(180);
-                    PHA_270(c2) = PHA_270(c2) + deg2rad(270);
-                    PHA_270(c3) = PHA_270(c3) + deg2rad(270);
-                    self.B1 = [MAG MAG MAG MAG] .* exp(1j * [PHA_000 PHA_090 PHA_180 PHA_270]);
-                    self.time = linspace(0, Tp*4, self.n_points.value*4);
-            end
+            PHA(c2) = PHA(c2) + deg2rad(self.delta_phase);
+            PHA(c3) = PHA(c3) + deg2rad(self.delta_phase);
+            self.time = t;
+            self.B1 = MAG .* exp(1j * PHA);
 
             self.GZ = ones(size(self.time)) * self.GZavg;
         end
@@ -117,9 +90,7 @@ classdef bir4_degraaf1995 < mri_rf_pulse_sim.backend.rf_pulse.abstract
         function init_specific_gui(self, container) % #abstract
             mri_rf_pulse_sim.ui_prop.scalar.add_uicontrol_multi_scalar(...
                 container,...
-                [self.gammaB1max, self.flip_angle, self.Beta, self.dWmax],...
-                [0 0 0.8 1]);
-            self.n_transients.add_uicontrol(container, [0.8 0 0.2 1])
+                [self.gammaB1max, self.flip_angle, self.Beta, self.dWmax]);
         end % fcn
 
     end % meths
