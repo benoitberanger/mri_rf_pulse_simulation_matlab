94e922785bd1c2bf35ea69dee5563369f05fb6e3
Benoît Béranger <benoit.beranger@icm-institute.org>
2024-05-05 20:17:14 +0200

    REF +analysis\evaluate_adiabaticity_hs.m

--- +mri_rf_pulse_sim/+analysis/evaluate_adiabaticity_hs.m
+++ +mri_rf_pulse_sim/+analysis/evaluate_adiabaticity_hs.m
@@ -1,13 +1,12 @@
-function varargout = evaluate_adiabaticity_hs()
-% This function evaluate the slice profile of HyperbolicSecant (hs) pulse
-% at different maximum RF amplitude
+function evaluate_adiabaticity_hs()
+%% What does it mean *adiabatic* ?
+% This function evaluate the slice profile of HyperbolicSecant (HS) pulse at different maximum RF amplitude ($B1_{max}$).
 
 
 %% Parameters
 
 % generate HS pulse with default parameters
 pulse = mri_rf_pulse_sim.rf_pulse.hs();
-pulse.plot();
 
 % set parameters of the solver
 solver = mri_rf_pulse_sim.bloch_solver();
@@ -19,6 +18,15 @@ solver.setDeltaB0(0); % in this example, assume no dB0
 % Evaluate slice profile over these deferent max amplitude :
 vect = 2 : 2 : 26; % µT
 b1max_range = mri_rf_pulse_sim.ui_prop.range(name='b1max', vect=vect*1e-6, unit='µT', scale=1e6);
+
+
+%% Plot pulse (default paramters)
+% In this plot, the $B1_{max}$ is the default value.
+% In the reste of the analysis, *only* the $B1_{max}$ will vary
+
+fig1 = figure(Name=sprintf('[%s]: %s', mfilename, pulse.summary), NumberTitle='off', Units='pixels', Position=[100 100 1600 800]);
+p1 = uipanel(Parent=fig1, Title=pulse.summary);
+pulse.plot(p1);
 
 
 %% Computation
@@ -50,35 +58,33 @@ efficiency_table.Properties.RowNames = {'efficiency (%)'};
 disp(efficiency_table)
 
 
-%% Plot
+%% Plot SliceProfile and Efficiency
 
-fig = figure('Name',mfilename,'NumberTitle','off');
+fig2 = figure(Name=sprintf('[%s]: SliceProfile & Efficiency', mfilename), NumberTitle='off', Units='pixels', Position=[100 100 1600 800]);
 
-ax1 = subplot(4,1, 1:3);
+ax(1) = subplot(4,1, 1:3, 'Parent',fig2);
+ax(2) = subplot(4,1,   4, 'Parent',fig2);
+hold(ax, 'all');
 colors = jet(b1max_range.N);
-hold(ax1, 'on');
+
+% Slice profile
 for idx = 1 : b1max_range.N
-    plot(ax1, solver.SpatialPosition.getScaled, all_slice_profile(:,idx), ...
+    plot(ax(1), solver.SpatialPosition.getScaled, all_slice_profile(:,idx), ...
         'Color', colors(idx,:), ...
         'DisplayName', sprintf('%g %s', b1max_range.vect(idx)*b1max_range.scale, b1max_range.unit), ...
         'LineWidth',2)
 end
-legend()
-xlabel('mm')
-ylabel('Mz')
+legend(ax(1))
+xlabel(ax(1),'mm')
+ylabel(ax(1),'M\mid\mid')
 
-ax2 = subplot(4,1, 4  );
-plot(ax2, b1max_range.getScaled(), efficiency, 'Marker', 'x', 'LineWidth',2);
-xlabel('µT')
-ylabel(efficiency_table.Properties.RowNames)
-
-
-%% Output ?
-
-if nargout
-    varargout{1} = efficiency_table;
-    varargout{2} = fig;
-end
+% Efficiency
+plot(ax(2), b1max_range.getScaled(), efficiency, 'Marker', 'x', 'LineWidth',2);
+xlabel(ax(2),'µT')
+ylabel(ax(2),efficiency_table.Properties.RowNames)
+xlim(ax(2),[min(b1max_range.getScaled()) max(b1max_range.getScaled())])
+xticks(ax(2),b1max_range.getScaled())
+grid(ax(2),'minor')
 
 
 end % fcn
