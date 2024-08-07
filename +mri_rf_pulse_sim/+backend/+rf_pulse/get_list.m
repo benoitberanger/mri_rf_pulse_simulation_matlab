c72f5359e9aca2ccf3cdc571a09a62a6449672f8
Benoît Béranger <benoit.beranger@icm-institute.org>
2024-08-07 17:16:53 +0200

    minor

--- +mri_rf_pulse_sim/+backend/+rf_pulse/get_list.m
+++ +mri_rf_pulse_sim/+backend/+rf_pulse/get_list.m
@@ -5,7 +5,7 @@ function pulse_list = get_list()
 base_path = which(class(mri_rf_pulse_sim.rf_pulse.sinc));
 package_path = fileparts(fileparts(base_path));
 
-% fetch all classes in the base_path : they are pulses
+% fetch all classes in the package_path : they are pulses
 dir_root = dir(fullfile(package_path, '@*'));
 pulse_list_root = {dir_root.name};
 pulse_list_root = strrep(pulse_list_root, '@', '');
@@ -17,10 +17,11 @@ for  i = 1 : length(dir_sub)
     dir_sub_current = dir(fullfile(dir_sub(i).folder, dir_sub(i).name, '@*'));
     pulse_list_sub_current = {dir_sub_current.name};
     pulse_list_sub_current = strrep(pulse_list_sub_current, '@', '');
-    pulse_list_sub = [pulse_list_sub strcat(dir_sub(i).name(2:end),filesep,pulse_list_sub_current)]; %#ok<AGROW> 
+    pulse_list_sub = [pulse_list_sub strcat(dir_sub(i).name(2:end),filesep,pulse_list_sub_current)]; %#ok<AGROW>
 end
 
-% TODO : use defined pulses
+% User defined pulses : they are in +mri_rf_pulse_sim/+rf_pulse/+local
+% This wat, the GUI will behave just the same !
 
 % concatenate all
 pulse_list = [pulse_list_root pulse_list_sub]';
