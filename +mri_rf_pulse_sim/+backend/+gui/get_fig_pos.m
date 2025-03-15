2823312959bc12ea63d8b79fba322cf073e7cf62
Benoît Béranger <benoit.beranger@icm-institute.org>
2025-03-15 14:10:47 +0100

    new gui elements positions

--- +mri_rf_pulse_sim/+backend/+gui/get_fig_pos.m
+++ +mri_rf_pulse_sim/+backend/+gui/get_fig_pos.m
@@ -16,10 +16,10 @@ end
 x1 = 1 - x0*2;
 y1 = 1 - y0*2;
 
-pulse_definition      = [0.0  0.2  0.3  0.8];
-simulation_parameters = [0.0  0.0  0.3  0.2];
-simulation_results    = [0.3  0.0  0.7  1.0];
-window                = [0.2  0.5  0.2  0.3];
+pulse_definition      = [0.00  0.20  0.40  0.80];
+simulation_parameters = [0.00  0.00  0.40  0.20];
+simulation_results    = [0.40  0.00  0.60  1.00];
+window                = [0.20  0.50  0.20  0.30];
 
 s.pulse_definition      = (pulse_definition      + [x0 y0 0 0]) .* [1 1 x1 y1];
 s.simulation_parameters = (simulation_parameters + [x0 y0 0 0]) .* [1 1 x1 y1];
