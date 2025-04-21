c2920b5ad678aaedbf3a84e1edd759eaaf748b74
Benoît Béranger <benoit.beranger@icm-institute.org>
2025-04-22 00:52:40 +0200

    sms_mb : time_shifted not working correctly
    but the idea is there...

--- +mri_rf_pulse_sim/+backend/+rf_pulse/@sms_mb/sms_mb.m
+++ +mri_rf_pulse_sim/+backend/+rf_pulse/@sms_mb/sms_mb.m
@@ -13,7 +13,8 @@ classdef (Abstract) sms_mb < handle
     properties (GetAccess = public, SetAccess = public)
         n_slice        mri_rf_pulse_sim.ui_prop.scalar                     % []  number of slices, from 1 to +Inf
         slice_distance mri_rf_pulse_sim.ui_prop.scalar                     % [m] distance between each slice
-        % time_shifted   mri_rf_pulse_sim.ui_prop.bool                       % [] time-shifted to reduce peak B1 (Auerbach 2013)
+        time_shifted   mri_rf_pulse_sim.ui_prop.bool                       % [] time-shifted to reduce peak B1 (Auerbach 2013)
+        temporal_shift mri_rf_pulse_sim.ui_prop.scalar                     % [] from 0 to +Inf
     end % props
 
     properties (GetAccess = public, SetAccess = protected, Dependent)
@@ -32,7 +33,8 @@ classdef (Abstract) sms_mb < handle
         function self = sms_mb()
             self.n_slice        = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='n_slice'       , value=3                                        );
             self.slice_distance = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='slice_distance', value=6 * 1e-3, unit='mm'           , scale=1e3);
-            % self.time_shifted   = mri_rf_pulse_sim.ui_prop.bool  (parent=self, name='time_shifted'  , value=false   , text='time_shifted'            );
+            self.time_shifted   = mri_rf_pulse_sim.ui_prop.bool  (parent=self, name='time_shifted'  , value=false   , text='time_shifted'            );
+            self.temporal_shift = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='temporal_shift', value=0.25                                     );
         end
 
         function mb_phase_modulation(self)
@@ -42,20 +44,45 @@ classdef (Abstract) sms_mb < handle
             % mb4->[-1.5 -0.5 +0.5 +1.5]
             offset_vect = (1:self.n_slice.get()) - (self.n_slice.get()+1)/2;
 
-            % compute the phase modution : each slice has its own delta of central frequency.
-            subpulse_b1 = zeros(self.n_slice.get(), self.n_points.get());
-            for idx = 1 : length(offset_vect)
-                mb_phase_modulation = exp(1j * 2*pi* self.band_seperation * offset_vect(idx) * self.time);
-                subpulse_b1(idx, :) = self.B1 .* mb_phase_modulation;
+            if self.time_shifted.get()
+                warning('!!! time shift NOT working yet !!!')
+
+                n_points_time_shifted = round(self.n_points * self.temporal_shift);
+                n_points_shift = round( self.n_points * (1 + self.temporal_shift*(self.n_slice-1)) );
+
+                subpulse_b1 = zeros(self.n_slice.get(), n_points_shift);
+                offset = 1;
+                for idx = 1 : length(offset_vect)
+                    mb_phase_modulation = exp(1j * 2*pi* self.band_seperation * offset_vect(idx) * self.time);
+                    time_shifted_idx = offset:(offset+self.n_points-1);
+                    subpulse_b1(idx, time_shifted_idx) = self.B1 .* mb_phase_modulation;
+                    offset = offset + n_points_time_shifted;
+                end
+                mb_B1 = sum(subpulse_b1, 1);
+                mb_time = linspace(self.time(1),self.time(end),length(mb_B1));
+
+                self.B1 = interp1(mb_time,mb_B1,self.time);
+
+            else
+
+                % compute the phase modution : each slice has its own delta of central frequency.
+                subpulse_b1 = zeros(self.n_slice.get(), self.n_points.get());
+                for idx = 1 : length(offset_vect)
+                    mb_phase_modulation = exp(1j * 2*pi* self.band_seperation * offset_vect(idx) * self.time);
+                    subpulse_b1(idx, :) = self.B1 .* mb_phase_modulation;
+                end
+                self.B1 = sum(subpulse_b1, 1);
+
             end
-            self.B1 = sum(subpulse_b1, 1);
+
 
         end % fcn
 
         function init_mb_gui(self, container)
-            self.n_slice       .add_uicontrol(container, [0.00 0.60 1.00 0.40])
-            self.slice_distance.add_uicontrol(container, [0.00 0.20 1.00 0.40])
-            % self.time_shifted  .add_uicontrol(container, [0.00 0.00 1.00 0.20])
+            self.n_slice       .add_uicontrol(container, [0.00 0.70 1.00 0.30])
+            self.slice_distance.add_uicontrol(container, [0.00 0.40 1.00 0.30])
+            self.time_shifted  .add_uicontrol(container, [0.00 0.20 1.00 0.20])
+            self.temporal_shift.add_uicontrol(container, [0.00 0.00 1.00 0.20])
         end % fcn
 
     end % meths
