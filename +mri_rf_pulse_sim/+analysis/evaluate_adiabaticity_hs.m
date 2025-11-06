cbb486f8d78b49a396d680424685675476c4ca5d
Benoît Béranger <benoit.beranger@icm-institute.org>
2025-11-06 11:29:54 +0100

    Update evaluate_adiabaticity_hs.m

--- +mri_rf_pulse_sim/+analysis/evaluate_adiabaticity_hs.m
+++ +mri_rf_pulse_sim/+analysis/evaluate_adiabaticity_hs.m
@@ -2,22 +2,28 @@ function evaluate_adiabaticity_hs()
 %% What does it mean *adiabatic* ?
 % This function evaluate the slice profile of HyperbolicSecant (HS) pulse at different maximum RF amplitude ($B1_{max}$).
 
+% evaluate pulse chemical shift profile in this ppm range
+target_range_db0 = 5; % ppm
+
+% Evaluate slice profile over these deferent max amplitude :
+vect = 1 : 1 : 30; % µT
+b1max_range = mri_rf_pulse_sim.ui_prop.range(name='b1max', vect=vect*1e-6, unit='µT', scale=1e6);
+
 
 %% Parameters
 
 % generate HS pulse with default parameters
 pulse = mri_rf_pulse_sim.rf_pulse.hs();
+pulse.bw.set(3000); % Hz
+pulse.slice_thickness.set(Inf); % set non-selective pulse
 
 % set parameters of the solver
 solver = mri_rf_pulse_sim.bloch_solver();
 solver.setPulse(pulse);
-n_dz = 301;
-solver.setSpatialPosition(linspace(-pulse.slice_thickness,+pulse.slice_thickness,n_dz));
+solver.setSpatialPosition(0); % non-selective pulse, don't look at off-center effects
 solver.setDeltaB0(0); % in this example, assume no dB0
-
-% Evaluate slice profile over these deferent max amplitude :
-vect = 2 : 2 : 26; % µT
-b1max_range = mri_rf_pulse_sim.ui_prop.range(name='b1max', vect=vect*1e-6, unit='µT', scale=1e6);
+n_db0 = 301;
+solver.setDeltaB0(linspace(-target_range_db0*4,+target_range_db0*4,n_db0)*1e-6);
 
 
 %% Plot pulse (default parameters)
@@ -32,8 +38,7 @@ pulse.plot(p1);
 %% Computation
 
 % pre-allocation
-all_slice_profile = zeros(solver.SpatialPosition.N,b1max_range.N);
-mid_slice_profile = zeros(1                       ,b1max_range.N);
+all_chemical_profile = zeros(solver.DeltaB0.N,b1max_range.N);
 
 for idx = 1 : b1max_range.N
     % update pulse (the solver has a reference to the pulse object)
@@ -42,14 +47,18 @@ for idx = 1 : b1max_range.N
 
     % solve and store
     solver.solve();
-    all_slice_profile(:,idx) = solver.getSliceProfilePara();
-    mid_slice_profile(idx)   = solver.getSliceMiddlePara();
+    all_chemical_profile(:,idx) = solver.getChemicalShiftPara();
 end
 
 
 %%  Display inversion efficiency
 
-efficiency = round(abs(mid_slice_profile-1)/2 *100); % convert Mz from [-1 to +1] into [0% to 100%]
+lower_bound = solver.DeltaB0.getScaled() >= -target_range_db0/2;
+upper_bound = solver.DeltaB0.getScaled() <= +target_range_db0/2;
+idx_bound = lower_bound & upper_bound;
+good_range = all_chemical_profile(idx_bound,:);
+
+efficiency = round(abs(mean(good_range,1)-1)/2 *100); % convert Mz from [-1 to +1] into [0% to 100%]
 
 % and now print efficiency in a nice way
 efficiency_table = array2table(efficiency);
@@ -58,24 +67,24 @@ efficiency_table.Properties.RowNames = {'efficiency (%)'};
 disp(efficiency_table)
 
 
-%% Plot SliceProfile and Efficiency
+%% Plot ChemicalProfile and Efficiency
 
-fig2 = figure(Name=sprintf('[%s]: SliceProfile & Efficiency', mfilename), NumberTitle='off', Units='pixels', Position=[100 100 1600 800]);
+fig2 = figure(Name=sprintf('[%s]: ChemicalProfile & Efficiency', mfilename), NumberTitle='off', Units='pixels', Position=[100 100 1600 800]);
 
 ax(1) = subplot(4,1, 1:3, 'Parent',fig2);
 ax(2) = subplot(4,1,   4, 'Parent',fig2);
 hold(ax, 'all');
 colors = jet(b1max_range.N);
 
-% Slice profile
+% Chemical profile
 for idx = 1 : b1max_range.N
-    plot(ax(1), solver.SpatialPosition.getScaled, all_slice_profile(:,idx), ...
+    plot(ax(1), solver.DeltaB0.getScaled(), all_chemical_profile(:,idx), ...
         'Color', colors(idx,:), ...
         'DisplayName', sprintf('%g %s', b1max_range.vect(idx)*b1max_range.scale, b1max_range.unit), ...
         'LineWidth',2)
 end
 legend(ax(1))
-xlabel(ax(1),'mm')
+xlabel(ax(1),'ppm')
 ylabel(ax(1),'M\mid\mid')
 
 % Efficiency
