445a28a8e33e8071320fdd0d8e961e891359c23c
Benoît Béranger <benoit.beranger@icm-institute.org>
2025-11-20 18:10:06 +0100

    minor refactoring

--- +mri_rf_pulse_sim/+rf_pulse/@DEMO_exc_ref/DEMO_exc_ref.m
+++ +mri_rf_pulse_sim/+rf_pulse/@DEMO_exc_ref/DEMO_exc_ref.m
@@ -7,8 +7,8 @@ classdef DEMO_exc_ref < mri_rf_pulse_sim.backend.rf_pulse.abstract
     end % props
 
     properties (GetAccess = public, SetAccess = protected)
-        exc__SLR             mri_rf_pulse_sim.rf_pulse.slr
-        ref__SLR             mri_rf_pulse_sim.rf_pulse.slr
+        exc                  mri_rf_pulse_sim.rf_pulse.slr
+        ref                  mri_rf_pulse_sim.rf_pulse.slr
     end % props
 
     methods (Access = public)
@@ -17,20 +17,20 @@ classdef DEMO_exc_ref < mri_rf_pulse_sim.backend.rf_pulse.abstract
         function self = DEMO_exc_ref()
             warning('!!! NOT A PULSE !!! mostly for demonstration purpose')
 
-            self.exc__SLR = mri_rf_pulse_sim.rf_pulse.slr();
-            self.exc__SLR.parent = self; % this allows the automatic GUI update
-            self.exc__SLR.pulse_type.value = "ex";
-            self.exc__SLR.filter_type.value = "ls";
-            self.exc__SLR.duration.set(2e-3);
-            self.exc__SLR.gz_rewinder.setTrue();
+            self.exc = mri_rf_pulse_sim.rf_pulse.slr();
+            self.exc.parent = self; % this allows the automatic GUI update
+            self.exc.pulse_type.value = "ex";
+            self.exc.filter_type.value = "ls";
+            self.exc.duration.set(2e-3);
+            self.exc.gz_rewinder.setTrue();
 
-            self.ref__SLR = mri_rf_pulse_sim.rf_pulse.slr();
-            self.ref__SLR.parent = self; % this allows the automatic GUI update
-            self.ref__SLR.pulse_type.value = "se";
-            self.ref__SLR.filter_type.value = "ls";
-            self.ref__SLR.flip_angle.set(180);
-            self.ref__SLR.rf_phase.set(90);
-            self.ref__SLR.slice_thickness.set(self.exc__SLR.slice_thickness*2);
+            self.ref = mri_rf_pulse_sim.rf_pulse.slr();
+            self.ref.parent = self; % this allows the automatic GUI update
+            self.ref.pulse_type.value = "se";
+            self.ref.filter_type.value = "ls";
+            self.ref.flip_angle.set(180);
+            self.ref.rf_phase.set(90);
+            self.ref.slice_thickness.set(self.exc.slice_thickness*2);
 
             self.crusher_dephasing    = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='crusher_dephasing', value=  8*pi, unit='rad', scale=1/pi);
             self.crusher_duration     = mri_rf_pulse_sim.ui_prop.scalar(parent=self, name='crusher_duration' , value=  1e-3, unit='ms' , scale=1e3 );
@@ -46,35 +46,35 @@ classdef DEMO_exc_ref < mri_rf_pulse_sim.backend.rf_pulse.abstract
         function generate_DEMO_exc_ref(self)
 
             % prep pulses
-            exc_rewinder = self.exc__SLR.gz_rewinder.get();
-            self.exc__SLR.gz_rewinder.setFalse();
-            self.exc__SLR.generate();
-            self.exc__SLR.gz_rewinder.set(exc_rewinder);
-            self.ref__SLR.generate();
+            exc_rewinder = self.exc.gz_rewinder.get();
+            self.exc.gz_rewinder.setFalse();
+            self.exc.generate();
+            self.exc.gz_rewinder.set(exc_rewinder);
+            self.ref.generate();
 
 
             % prep timings and index
             self.duration.value = self.get_DEMO_ref_duration();
-            self.time = linspace(-self.exc__SLR.duration/2, self.TE.get(), (self.exc__SLR.n_points+self.ref__SLR.n_points)*2);
-            exc_idx = self.time < self.exc__SLR.duration/2;
-            rew_idx = (self.time >= self.exc__SLR.duration/2) & (self.time <= self.exc__SLR.duration*1);
-            ref_idx = (self.time >= self.TE/2-self.ref__SLR.duration/2) & (self.time <= self.TE/2+self.ref__SLR.duration/2);
+            self.time = linspace(-self.exc.duration/2, self.TE.get(), (self.exc.n_points+self.ref.n_points)*2);
+            exc_idx = self.time < self.exc.duration/2;
+            rew_idx = (self.time >= self.exc.duration/2) & (self.time <= self.exc.duration*1);
+            ref_idx = (self.time >= self.TE/2-self.ref.duration/2) & (self.time <= self.TE/2+self.ref.duration/2);
 
-            exc__time = linspace(-self.exc__SLR.duration/2, +self.exc__SLR.duration/2, sum(exc_idx));
-            ref__time = linspace(-self.ref__SLR.duration/2, +self.ref__SLR.duration/2, sum(ref_idx));
+            exc__time = linspace(-self.exc.duration/2, +self.exc.duration/2, sum(exc_idx));
+            ref__time = linspace(-self.ref.duration/2, +self.ref.duration/2, sum(ref_idx));
 
-            exc__waveform = interp1(self.exc__SLR.time,self.exc__SLR.B1, exc__time, 'spline');
-            ref__waveform = interp1(self.ref__SLR.time,self.ref__SLR.B1, ref__time, 'spline');
+            exc__waveform = interp1(self.exc.time,self.exc.B1, exc__time, 'spline');
+            ref__waveform = interp1(self.ref.time,self.ref.B1, ref__time, 'spline');
 
-            exc__grad     = interp1(self.exc__SLR.time,self.exc__SLR.GZ, exc__time, 'spline');
-            ref__grad     = interp1(self.ref__SLR.time,self.ref__SLR.GZ, ref__time, 'spline');
+            exc__grad     = interp1(self.exc.time,self.exc.GZ, exc__time, 'spline');
+            ref__grad     = interp1(self.ref.time,self.ref.GZ, ref__time, 'spline');
 
             self.B1 = zeros(size(self.time));
             self.GZ = zeros(size(self.time));
 
             self.B1(exc_idx) = self.B1(exc_idx) + exc__waveform;
             self.GZ(exc_idx) = self.GZ(exc_idx) + exc__grad;
-            if self.exc__SLR.gz_rewinder
+            if self.exc.gz_rewinder
                 [~,waveform_max_idx] = max(abs(exc__waveform));
                 asymmetry = 1 - (waveform_max_idx / length(exc__waveform));
                 self.GZ(rew_idx) = self.GZ(rew_idx) -exc__grad(1:sum(rew_idx)) * (asymmetry/0.5); % quick and dirty way to have correct phase
@@ -83,16 +83,16 @@ classdef DEMO_exc_ref < mri_rf_pulse_sim.backend.rf_pulse.abstract
             self.GZ(ref_idx) = self.GZ(ref_idx) + ref__grad;
 
             % prep crushers
-            crusher_moment = self.crusher_dephasing / (self.gamma * self.exc__SLR.slice_thickness);
+            crusher_moment = self.crusher_dephasing / (self.gamma * self.exc.slice_thickness);
             crusher_ampl   = crusher_moment / self.crusher_duration;
-            crusher_L_idx  = (self.time > self.TE/2-self.ref__SLR.duration/2-self.crusher_duration) & (self.time < self.TE/2-self.ref__SLR.duration/2);
-            crusher_R_idx  = (self.time > self.TE/2+self.ref__SLR.duration/2) & (self.time < self.TE/2+self.ref__SLR.duration/2+self.crusher_duration);
+            crusher_L_idx  = (self.time > self.TE/2-self.ref.duration/2-self.crusher_duration) & (self.time < self.TE/2-self.ref.duration/2);
+            crusher_R_idx  = (self.time > self.TE/2+self.ref.duration/2) & (self.time < self.TE/2+self.ref.duration/2+self.crusher_duration);
             self.GZ(crusher_L_idx) = self.GZ(crusher_L_idx) + crusher_ampl;
             self.GZ(crusher_R_idx) = self.GZ(crusher_R_idx) + crusher_ampl;
         end % fcn
 
         function value = get_bandwidth(self) % #abstract
-            value = self.exc__SLR.get_bandwidth();
+            value = self.exc.get_bandwidth();
         end % fcn
 
         function txt = summary(self) % #abstract
@@ -115,10 +115,10 @@ classdef DEMO_exc_ref < mri_rf_pulse_sim.backend.rf_pulse.abstract
                 'Position',pos_ref,...
                 'BackgroundColor',fig_col.figureBG);
 
-            self.ref__SLR.gz_rewinder.visible = "off";
+            self.ref.gz_rewinder.visible = "off";
 
-            self.exc__SLR.init_base_gui(panel_exc);
-            self.ref__SLR.init_base_gui(panel_ref);
+            self.exc.init_base_gui(panel_exc);
+            self.ref.init_base_gui(panel_ref);
         end % fcn
 
         function init_specific_gui(self, container) % #abstract
@@ -143,8 +143,8 @@ classdef DEMO_exc_ref < mri_rf_pulse_sim.backend.rf_pulse.abstract
                 'Position',pos_oth,...
                 'BackgroundColor',fig_col.figureBG);
 
-            self.exc__SLR.init_specific_gui(panel_exc);
-            self.ref__SLR.init_specific_gui(panel_ref);
+            self.exc.init_specific_gui(panel_exc);
+            self.ref.init_specific_gui(panel_ref);
 
             mri_rf_pulse_sim.ui_prop.scalar.add_uicontrol_multi_scalar(...
                 panel_oth,...
@@ -157,7 +157,7 @@ classdef DEMO_exc_ref < mri_rf_pulse_sim.backend.rf_pulse.abstract
     methods(Access = protected)
 
         function value = get_DEMO_ref_duration(self)
-            value = self.TE + self.exc__SLR.duration/2;
+            value = self.TE + self.exc.duration/2;
         end % fcn
 
     end % meths
